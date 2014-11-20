1fee2e343f818ae088b65e78a57c2272011991a6
Federico Barrios <fbarrios@fi.uba.ar>
2014-11-19 22:26:15 -0300

    Working on the script to run ROUGE automatically.

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -1,9 +1,19 @@
 import os
 import os.path
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 
 from pyrouge import Rouge155
+from textrank.textrank import textrank
 
 ROUGE_PATH = os.path.join(os.getcwd(), 'ROUGE-RELEASE-1.5.5')
 Rouge155(ROUGE_PATH)
 
-from pyrouge import test
+with open('datasets/cmplg/001/text.txt') as fp:
+    text = fp.read()
+
+summary = textrank(text)
+
+with open('temp.txt', 'w') as fp:
+    fp.write(summary)
+
