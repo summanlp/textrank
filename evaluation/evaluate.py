161d6861984c1851898e0d39c5dba0a3a890dc28
Federico Barrios <fbarrios@fi.uba.ar>
2014-11-19 23:44:45 -0300

    Script for running pyrouge working.

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -1,19 +1,36 @@
 import os
 import os.path
 import sys
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
-
 from pyrouge import Rouge155
+
+# Needed to import files from a parent folder.
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 from textrank.textrank import textrank
 
+
+""" Script that runs ROUGE to compare the output of a custom summarization tool
+    comparing it to a 'gold standard' reference summary.
+"""
+
+# Initializes ROUGE.
 ROUGE_PATH = os.path.join(os.getcwd(), 'ROUGE-RELEASE-1.5.5')
-Rouge155(ROUGE_PATH)
+rouge_instance = Rouge155(ROUGE_PATH)
 
-with open('datasets/cmplg/001/text.txt') as fp:
+# pyrouge needs the model summaries to be stored in a directory without subdirectories.
+if not os.path.exists('temp'):
+    os.makedirs('temp')
+
+# Makes a summary of the provided file and stores it in the temp folder.
+with open('datasets/elhadad/06/text.txt') as fp:
     text = fp.read()
-
 summary = textrank(text)
-
-with open('temp.txt', 'w') as fp:
+with open('temp/temp.txt', 'w') as fp:
     fp.write(summary)
 
+# Runs ROUGE comparing the gold reference summaries with the recently generated.
+rouge_instance.system_dir = 'datasets/elhadad/06'
+rouge_instance.system_filename_pattern = 'summ(\d+).txt'
+rouge_instance.model_dir = 'temp'
+rouge_instance.model_filename_pattern = 'temp.txt'
+
+print rouge_instance.convert_and_evaluate()
