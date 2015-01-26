76cba3c7ee1adacade8499384e5b5f0319b241a0
Federico Lopez <felopez@despegar.com>
2015-01-26 15:07:16 -0300

    Adding setup files for upload to PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+# Instructions from http://peterdowns.com/posts/first-time-with-pypi.html
+setup(
+  name = 'summanlp',
+  packages = ['summanlp'],
+  version = '0.1',
+  description = 'A text summarization and keyword extraction lib',
+  author = 'Federico Barrios, Federico Lopez',
+  author_email = 'fbarrios@live.com, fedelopez77@gmail.com',
+  url = 'https://github.com/summanlp/textrank-module',
+  download_url = 'https://github.com/summanlp/textrank-module/tarball/0.1', 
+  keywords = ['textrank', 'nlp', 'summarization', "NLP", "natural language processing", "automatic summarization"]
+#  classifiers = []
+)
