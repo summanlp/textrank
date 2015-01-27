058f469879c22e07f1bef369fc6acc68e6b59c5a
Federico Barrios <fbarrios@fi.uba.ar>
2015-01-27 00:37:46 -0300

    Adding pagerank weighted comparison.

---
+++ textrank/test/pagerank_weighted_comparison.py
@@ -0,0 +1,8 @@
+from textrank.textcleaner import get_sentences
+from textrank.impl import get_graph, set_graph_edge_weights
+from textrank.pagerank_weighted import pagerank_weighted, pagerank_weighted_scipy
+
+"""Performs a comparison between the iterative version and the
+   scipy design."""
+
+
