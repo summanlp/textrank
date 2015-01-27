0bc574d5c6c63977192b283f8bc5bd1b52f8c729
Federico Barrios <fbarrios@fi.uba.ar>
2015-01-27 01:32:20 -0300

    Spaces for tabs.

--- textrank/textcleaner.py
+++ textrank/textcleaner.py
@@ -41,58 +41,58 @@ STOPWORDS = frozenset(w for w in STOPWORDS.split() if w)
 
 
 def tokenize_by_sentences(text):
-	"""
+    """
     Given some text, tokenizes into sentences, applies filters and lemmatizes them.
     Returns dictionary that map processed sentences to the originals sentences.
     """
-	original_sentences = split_sentences(text)
-	filtered_sentences = filter_words(original_sentences)
-	return {item[0]: item[1] for item in zip(filtered_sentences, original_sentences) if item[0]}
+    original_sentences = split_sentences(text)
+    filtered_sentences = filter_words(original_sentences)
+    return {item[0]: item[1] for item in zip(filtered_sentences, original_sentences) if item[0]}
 
 
 def split_sentences(text):
-	processed = replace_abbreviations(text)
-	return [undo_replacement(sentence) for sentence in get_sentences(processed)]
+    processed = replace_abbreviations(text)
+    return [undo_replacement(sentence) for sentence in get_sentences(processed)]
 
 
 def replace_abbreviations(text):
-	return replace_with_separator(text, SEPARATOR, [AB_SENIOR, AB_ACRONYM])
+    return replace_with_separator(text, SEPARATOR, [AB_SENIOR, AB_ACRONYM])
 
 
 def undo_replacement(sentence):
-	return replace_with_separator(sentence, r" ", [UNDO_AB_SENIOR, UNDO_AB_ACRONYM])
+    return replace_with_separator(sentence, r" ", [UNDO_AB_SENIOR, UNDO_AB_ACRONYM])
 
 
 def replace_with_separator(text, separator, regexs):
-	replacement = r"\1" + separator + r"\2"
-	result = text
-	for regex in regexs:
-		result = regex.sub(replacement, result)
-	return result
+    replacement = r"\1" + separator + r"\2"
+    result = text
+    for regex in regexs:
+        result = regex.sub(replacement, result)
+    return result
 
 
 def get_sentences(text):
-	for match in RE_SENTENCE.finditer(text):
-		yield match.group()
+    for match in RE_SENTENCE.finditer(text):
+        yield match.group()
 
 
 def filter_words(tokens):
-	filters = [lambda x: x.lower(), strip_numeric, strip_punctuation, remove_stopwords,
-			   stem_sentence]
-	# filters = []
+    filters = [lambda x: x.lower(), strip_numeric, strip_punctuation, remove_stopwords,
+               stem_sentence]
+    # filters = []
 
-	apply_filters_to_token = lambda token: apply_filters(token, filters)
-	return map(apply_filters_to_token, tokens)
+    apply_filters_to_token = lambda token: apply_filters(token, filters)
+    return map(apply_filters_to_token, tokens)
 
 
 def apply_filters(sentence, filters):
-	for f in filters:
-		sentence = f(sentence)
-	return sentence
+    for f in filters:
+        sentence = f(sentence)
+    return sentence
 
 
 def remove_stopwords(sentence):
-	return " ".join(w for w in sentence.split() if w not in STOPWORDS)
+    return " ".join(w for w in sentence.split() if w not in STOPWORDS)
 
 
 language = "english"
@@ -100,8 +100,8 @@ stemmer = snowball.SnowballStemmer(language)
 
 
 def stem_sentence(sentence):
-	word_stems = [stemmer.stem(word) for word in sentence.split()]
-	return " ".join(word_stems)
+    word_stems = [stemmer.stem(word) for word in sentence.split()]
+    return " ".join(word_stems)
 
 
 # Falta bocha por aca:
@@ -109,9 +109,9 @@ def stem_sentence(sentence):
 #  - Ver que se debe hacer con las palabras repetidas (releer paper)
 
 def tokenize_by_words(text):
-	# pdb.set_trace()
-	original_words = list(tokenize(text, to_lower=True))
-	
-	filters = [strip_numeric, strip_punctuation, remove_stopwords, stem_sentence]
-	tokens = { word: apply_filters(word, filters) for word in original_words }	
-	return { key: tokens[key] for key in tokens if tokens[key] != "" }
+    # pdb.set_trace()
+    original_words = list(tokenize(text, to_lower=True))
+    
+    filters = [strip_numeric, strip_punctuation, remove_stopwords, stem_sentence]
+    tokens = { word: apply_filters(word, filters) for word in original_words }	
+    return { key: tokens[key] for key in tokens if tokens[key] != "" }
