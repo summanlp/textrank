c19b9bbf0b97885bbce74ed1e1a1e6a76e3e8d2e
Federico Barrios <fbarrios@fi.uba.ar>
2015-01-27 00:48:29 -0300

    Adding test texts.

--- textrank/test/pagerank_weighted_comparison.py
+++ textrank/test/pagerank_weighted_comparison.py
@@ -1,8 +1,19 @@
+import sys
+import os
+from pygraph.classes.digraph import digraph as pydigraph
+
+sys.path.append(os.pardir)
+from textrank import textrank
+
 from textrank.textcleaner import get_sentences
 from textrank.impl import get_graph, set_graph_edge_weights
 from textrank.pagerank_weighted import pagerank_weighted, pagerank_weighted_scipy
 
 """Performs a comparison between the iterative version and the
    scipy design."""
+
+# Uses the summa database texts.
+TEXT_FILENAME_FORMAT = '../../evaluation/datasets/summa/{text_number:02d}/text.txt'
+TEXT_FILE_PATHS = [TEXT_FILENAME_FORMAT.format(text_number=i) for i in xrange(1, 11)]
 
 
