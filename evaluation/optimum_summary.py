d6c9935fcac9b00d6ae2165a7378a3a6400122ac
Federico Barrios <fbarrios@fi.uba.ar>
2015-02-17 19:18:50 -0300

    Finished with the best summary logic.

--- evaluation/optimum_summary.py
+++ evaluation/optimum_summary.py
@@ -2,6 +2,7 @@
 import sys
 import os
 from itertools import combinations
+from rouge_calculator import evaluate_summary
 from rouge_calculator import MODEL_DIRECTORY
 from rouge_calculator import MODEL_FILENAME
 from rouge_calculator import TEXT_FILENAME_FORMAT
@@ -13,6 +14,7 @@ from textcleaner import get_sentences
 
 SUMMARY_LENGHT = 0.2
 DATASET = 'summa'
+OPTIMUM_FILENAME_FORMAT = 'results/opt_{text_number:02d}.txt'
 
 
 def get_sentences_from_text(text_number):
@@ -22,23 +24,47 @@ def get_sentences_from_text(text_number):
     return list(get_sentences(text))
 
 
+def get_score_for_summay(text_number, summary):
+    with open(os.path.join(MODEL_DIRECTORY, MODEL_FILENAME), 'w') as fp:
+        fp.write(summary)
+
+    gold_references_dir = GOLD_REFERENCES_DIR_FORMAT.format(dataset=DATASET, text_number=text_number)
+    return evaluate_summary(gold_references_dir)
+
+
 def get_optimum_summary(text_number):
     """ Creates the best possible summary of a set length trying
     all posible combinations.
     """
 
     # Temporary stores the best results so far.
-    best_sentences = None
-    best_scores = None
+    best_summary = None
+    best_score = 0
 
     sentences = get_sentences_from_text(text_number)
     summary_lenght = int(len(sentences) * SUMMARY_LENGHT)
+
     # Creates a summary for each combination of sentences.
     for combination in combinations(sentences, summary_lenght):
-        #print combination
-        pass
+        summary = "\n".join(combination)
+        result = get_score_for_summay(text_number, summary)
 
-    # gold_references_dir = GOLD_REFERENCES_DIR_FORMAT.format(dataset=DATASET, text_number=text_number)
+        # We consider the average of several scores.
+        score = (result['rouge_1_f_score'] + result['rouge_2_f_score'] + result['rouge_su*_f_score']) / 3
+
+        if score > best_score:
+            best_score = score
+            best_summary = summary
+
+        print "summary with score", score, ". best score so far:", best_score
+        print ""
+        print ""
+        print ""
+
+    # The optimum summary is written to hard disk.
+    output_filename = OPTIMUM_FILENAME_FORMAT.format(text_number=text_number)
+    with open(output_filename, 'w') as fp:
+        fp.write(best_summary)
 
 
 get_optimum_summary(1)
