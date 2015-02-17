bf9c6af7e29ded51b7adabefcfd01bdcc4544cb8
Federico Barrios <fbarrios@fi.uba.ar>
2015-02-17 18:06:21 -0300

    Adding combinations.

--- evaluation/optimum_summary.py
+++ evaluation/optimum_summary.py
@@ -1,12 +1,26 @@
 
+import sys
+import os
 from itertools import combinations
 from rouge_calculator import MODEL_DIRECTORY
 from rouge_calculator import MODEL_FILENAME
 from rouge_calculator import TEXT_FILENAME_FORMAT
 from rouge_calculator import GOLD_REFERENCES_DIR_FORMAT
 
+# Imports files from a parent directory.
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, 'textrank'))
+from textcleaner import get_sentences
+
 SUMMARY_LENGHT = 0.2
 DATASET = 'summa'
+
+
+def get_sentences_from_text(text_number):
+    text_filename = TEXT_FILENAME_FORMAT.format(dataset=DATASET, text_number=text_number)
+    with open(text_filename) as fp:
+        text = fp.read()
+    return list(get_sentences(text))
+
 
 def get_optimum_summary(text_number):
     """ Creates the best possible summary of a set length trying
@@ -17,11 +31,14 @@ def get_optimum_summary(text_number):
     best_sentences = None
     best_scores = None
 
-    
-    text_filename = TEXT_FILENAME_FORMAT.format(dataset=DATASET, text_number=text_number)
+    sentences = get_sentences_from_text(text_number)
+    summary_lenght = int(len(sentences) * SUMMARY_LENGHT)
+    # Creates a summary for each combination of sentences.
+    for combination in combinations(sentences, summary_lenght):
+        #print combination
+        pass
+
+    # gold_references_dir = GOLD_REFERENCES_DIR_FORMAT.format(dataset=DATASET, text_number=text_number)
 
 
-
-    gold_references_dir = GOLD_REFERENCES_DIR_FORMAT.format(dataset=DATASET, text_number=text_number)
-
-
+get_optimum_summary(1)
