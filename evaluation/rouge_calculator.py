cf32ef33a7a4137666b6b86ab7984e778e5c0864
Federico Barrios <fbarrios@fi.uba.ar>
2015-02-22 19:43:23 -0300

    Finished with the merge.

--- evaluation/rouge_calculator.py
+++ evaluation/rouge_calculator.py
@@ -1,5 +1,6 @@
 import os
 import os.path
+from tempfile import mkdtemp
 from evaluation_constants import GOLD_REFERENCES_DIR_FORMAT
 from evaluation_constants import GOLD_REFERENCES_PATTERN
 from pyrouge import Rouge155
@@ -10,34 +11,56 @@ from pyrouge import Rouge155
 
 ROUGE_PATH = os.path.join(os.getcwd(), 'ROUGE-RELEASE-1.5.5')
 ROUGE_DATA_PATH = os.path.join(ROUGE_PATH, 'data')
+SYSTEM_DIR = "system"
+MODEL_DIR = "model"
+CONFIG_FILENAME = "config.xml"
 
 ROUGE_OPTIONS = [
-    '-e', ROUGE_DATA_PATH,  # Specify ROUGE_EVAL_HOME directory where the ROUGE data files can be found.
-    '-n', '2',              # Compute ROUGE-1 and ROUGE-2.
-    '-x',                   # Do not calculate ROUGE-L.
-    '-m',                   # Apply Porter stemmer on both models and peers.
-    '-2', '4',              # Compute skip bigram (ROGUE-S) co-occurrence with a maximum skip distance of 4,
-    '-u',                   # Include unigram in Skip Bigram (ROUGE-S).
-    '-c', '95',             # Specify CF\% (0 <= CF <= 100) confidence interval to compute.
-    '-r', '1000',           # Specify the number of sampling point in bootstrap resampling (default is 1000).
-    '-f', 'A',              # Scores are averaged over multiple models.
-    '-p', '0.5',            # Compute F-measure with alpha = 0.5.
-    '-t', '0',              # Use model unit as the counting unit.
-    '-a'                    # Evaluate all systems.
+    '-e', ROUGE_DATA_PATH, # Specify ROUGE_EVAL_HOME directory where the ROUGE data files can be found.
+    '-c', '95', # Specify CF\% (0 <= CF <= 100) confidence interval to compute.
+    '-2', # Compute skip bigram (ROGUE-S) co-occurrence,
+    '-1',
+    '-U', # Compute skip bigram as -2 but include unigram.
+    '-r', '1000', # Specify the number of sampling point in bootstrap resampling (default is 1000).
+    '-n', '2', # Compute ROUGE-N up to max-ngram length will be computed.
+    '-a', # Evaluate all systems specified in the ROUGE-eval-config-file.
+    '-x' # Do not calculate ROUGE-L.
 ]
 
 
-def evaluate_summary(text_number, dataset, model_directory, model_filename):
-    rouge_instance = Rouge155(ROUGE_PATH, ' '.join(ROUGE_OPTIONS))
+def create_temporary_directories():
+    tempdir = mkdtemp()
 
-    gold_references_dir = GOLD_REFERENCES_DIR_FORMAT.format(dataset=dataset, text_number=text_number)
+    # Creates the temp directories to hold the rouge files.
+    new_system_dir = os.path.join(tempdir, SYSTEM_DIR)
+    os.mkdir(new_system_dir)
+    new_model_dir = os.path.join(tempdir, MODEL_DIR)
+    os.mkdir(new_model_dir)
+
+    return tempdir
+
+
+def evaluate_summary(text_number, dataset, model_directory, model_filename):
+    tempdir = create_temporary_directories()
+
+    rouge_instance = Rouge155(ROUGE_PATH, verbose=False, rouge_args=' '.join(ROUGE_OPTIONS))
+
+    # Converts the gold references files to rouge format.
+    gold_references_input_dir = GOLD_REFERENCES_DIR_FORMAT.format(dataset=dataset, text_number=text_number)
+    gold_references_output_dir = os.path.join(tempdir, SYSTEM_DIR)
+    rouge_instance.convert_summaries_to_rouge_format(gold_references_input_dir, gold_references_output_dir)
+
+    # Converts the summary file to rouge format.
+    model_directory_output_dir = os.path.join(tempdir, MODEL_DIR)
+    rouge_instance.convert_summaries_to_rouge_format(model_directory, model_directory_output_dir)
+
+    # Writes the configuration file.
+    config_filename = os.path.join(tempdir, CONFIG_FILENAME)
+    rouge_instance.write_config_static(gold_references_output_dir, GOLD_REFERENCES_PATTERN,
+                                       model_directory_output_dir, model_filename,
+                                       config_filename, 1)
 
     # Runs ROUGE comparing the gold reference summaries with the recently generated.
-    rouge_instance.system_dir = gold_references_dir
-    rouge_instance.system_filename_pattern = GOLD_REFERENCES_PATTERN
-    rouge_instance.model_dir = model_directory
-    rouge_instance.model_filename_pattern = model_filename
-
-    output = rouge_instance.convert_and_evaluate()
+    output = rouge_instance.evaluate_static(ROUGE_PATH, config_filename, ROUGE_OPTIONS)
 
     return rouge_instance.output_to_dict(output)
