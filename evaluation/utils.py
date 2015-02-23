ce3e7fa412c72ee52583076e08c1603ef5ea8b01
Federico Barrios <fbarrios@fi.uba.ar>
2015-02-22 22:56:27 -0300

    Directory listing is now sorted.

--- evaluation/utils.py
+++ evaluation/utils.py
@@ -5,5 +5,6 @@ from os.path import join
 
 
 def get_directories_from_path(path):
-    return [directory for directory in listdir(path) if isdir(join(path, directory))]
+    directories = [directory for directory in listdir(path) if isdir(join(path, directory))]
+    return sorted(directories)
 
