1fc3eb9171faa98792fbb631d1835ca28c658940
fedelopez77 <fedelopez77@gmail.com>
2015-03-09 23:24:27 -0300

    Adding longest common substring comparison

--- textrank/textrank_sentence.py
+++ textrank/textrank_sentence.py
@@ -64,32 +64,21 @@ def set_graph_edge_weights(graph):
 
 
 def get_similarity(s1, s2):
-    return len(lcs(s1, s2))
+    return len(longest_common_substring(s1, s2))
 
-def lcs(a, b):
-    lengths = [[0 for j in range(len(b)+1)] for i in range(len(a)+1)]
-    # row 0 and column 0 are initialized to 0 already
-    for i, x in enumerate(a):
-        for j, y in enumerate(b):
-            if x == y:
-                lengths[i+1][j+1] = lengths[i][j] + 1
+def longest_common_substring(s1, s2):
+    m = [[0] * (1 + len(s2)) for i in xrange(1 + len(s1))]
+    longest, x_longest = 0, 0
+    for x in xrange(1, 1 + len(s1)):
+        for y in xrange(1, 1 + len(s2)):
+            if s1[x - 1] == s2[y - 1]:
+                m[x][y] = m[x - 1][y - 1] + 1
+                if m[x][y] > longest:
+                    longest = m[x][y]
+                    x_longest = x
             else:
-                lengths[i+1][j+1] = \
-                    max(lengths[i+1][j], lengths[i][j+1])
-    # read the substring out from the matrix
-    result = ""
-    x, y = len(a), len(b)
-    while x != 0 and y != 0:
-        if lengths[x][y] == lengths[x-1][y]:
-            x -= 1
-        elif lengths[x][y] == lengths[x][y-1]:
-            y -= 1
-        else:
-            assert a[x-1] == b[y-1]
-            result = a[x-1] + result
-            x -= 1
-            y -= 1
-    return result
+                m[x][y] = 0
+    return s1[x_longest - longest: x_longest]
 
 
 def get_test_graph(path):
