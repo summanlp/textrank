e27d2484d1787577d0405becaad82b362d7eddee
Federico Barrios <fbarrios@fi.uba.ar>
2018-05-02 10:26:57 +0200

    Preparing for 1.0.0 release.

--- setup.py
+++ setup.py
@@ -6,15 +6,17 @@ setup(
     package_data = {
         'summa': ['README', 'LICENSE']
     },
-    version = '0.1.0',
-    description = 'A text summarization and keyword extraction package based on textrank',
-    long_description=open('README').read(),
+    version = '1.0.0',
+    description = 'A text summarization and keyword extraction package based on TextRank',
+    long_description = open('README').read(),
     author = 'Federico Barrios, Federico Lopez',
     author_email = 'summanlp@gmail.com',
     url = 'https://github.com/summanlp/textrank',
-    download_url = 'https://github.com/summanlp/textrank/tarball/v0.1.0',
-    keywords = ['summa', 'nlp', 'summarization', "NLP", "natural language processing", "automatic summarization",
-        "keywords", "summary", "textrank", "pagerank"],
+    download_url = 'https://github.com/summanlp/textrank/tarball/v1.0.0',
+    keywords = ['summa', 'nlp', 'summarization', "NLP",
+                "natural language processing",
+                "automatic summarization",
+                "keywords", "summary", "textrank", "pagerank"],
     install_requires = [
         'scipy >= 0.19'
     ],
@@ -32,8 +34,8 @@ setup(
 
         'Programming Language :: Python :: 3 :: Only'
     ],
-    test_suite="test",
-    entry_points={
+    test_suite = "test",
+    entry_points = {
        'console_scripts': [
            'textrank = summa.textrank:main',
        ],
