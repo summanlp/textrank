2cfa7bfc637ec58a6b0c380396fc479756616545
ijmermet <ijmermet@gmail.com>
2018-08-23 12:09:35 -0300

    migration to argparse

--- summa/textrank.py
+++ summa/textrank.py
@@ -1,4 +1,6 @@
-import sys, getopt
+import os, sys
+import argparse
+
 
 from .summarizer import summarize
 from .keywords import keywords
@@ -8,73 +10,41 @@ SENTENCE = 0
 WORD = 1
 
 
-def exit_with_error(err):
-    print("Error: " + str(err))
-    usage()
-    sys.exit(2)
-
-def get_arguments():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "t:s:r:w:h", ["text=", "summary=", "ratio=", "words=", "help"])
-    except getopt.GetoptError as err:
-        exit_with_error(err)
-
-    path = None
-    summarize_by = SENTENCE
-    ratio = 0.2
-    words = None
-    for o, a in opts:
-        if o in ("-t", "--text"):
-            path = a
-        elif o in ("-h", "--help"):
-            usage()
-            sys.exit()
-        elif o in ("-s", "--summary"):
-            summarize_by = int(a)
-        elif o in ("-w", "--words"):
-            words = int(a)
-        elif o in ("-r", "--ratio"):
-            ratio = float(a)
-        else:
-            assert False, "unhandled option"
-
-    if path is None:
-        exit_with_error("-t option is required.")
-
-    return path, summarize_by, ratio, words
-
-
-help_text = """Usage: textrank -t FILE
--s UNIT, --summary=UNIT:
-\tType of unit to summarize: sentence (0) or word (1). Default value: 0
-\t0: Sentence. 1: Word
--t FILE, --text=FILE:
-\tPATH to text to summarize
--r RATIO, --ratio=RATIO:
-\tFloat number (0,1] that defines the length of the summary. It's a proportion of the original text. Default value: 0.2.
--w WORDS, --words=WORDS:
-\tNumber to limit the length of the summary. The length option is ignored if the word limit is set.
--h, --help:
-\tprints this help
-"""
-def usage():
-    print(help_text)
-
-
-def textrank(text, summarize_by=SENTENCE, ratio=0.2, words=None):
+def textrank(text, summarize_by=SENTENCE, ratio=0.2, words=None, additional_stopwords=None):
     if summarize_by == SENTENCE:
-        return summarize(text, ratio, words)
+        return summarize(text, ratio, words, additional_stopwords=additional_stopwords)
     else:
-        return keywords(text, ratio, words)
+        return keywords(text, ratio, words, additional_stopwords=additional_stopwords)
+
+
+def restricted_float(x):
+    x = float(x)
+    if x < 0.0 or x > 1.0:
+        raise argparse.ArgumentTypeError("%r not in range [0.0, 1.0]".format(x,))
+    return x
 
 
 def main():
-    path, summarize_by, ratio, words = get_arguments()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--summary', '-s', type=int, default=0, help="Type of unit to summarize: sentence (0) or word (1)")
+    parser.add_argument('--text', '-t', type=str, help="Text to summarize")
+    parser.add_argument('--ratio', '-r', type=restricted_float, default=0.2, help="Float number (0,1] that defines the length of the summary. It's a proportion of the original text")
+    parser.add_argument('--words', '-w', help="Number to limit the length of the summary. The length option is ignored if the word limit is set.")
+    parser.add_argument('--additional_stopwords', '-a', help="Either a string of comma separated stopwords or a path to a file which has comma separated stopwords in every line")
+    args = parser.parse_args()
 
-    with open(path) as file:
+    with open(args.text) as file:
         text = file.read()
 
-    print(textrank(text, summarize_by, ratio, words))
+    additional_stopwords = None
+    if args.additional_stopwords:
+        if os.path.exists(args.additional_stopwords):
+            with open(args.additional_stopwords) as f:
+                additional_stopwords = {s for l in f for s in l.strip().split(",")}
+        else:
+            additional_stopwords = args.additional_stopwords.split(",")
+
+    print(textrank(text, args.summary, args.ratio, args.words, additional_stopwords))
 
 
 if __name__ == "__main__":
