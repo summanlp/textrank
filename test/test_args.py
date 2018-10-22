b3d8fec95bc8b2691951c50fc6555751bce5bed4
Federico Barrios <fbarrios@fi.uba.ar>
2018-10-22 19:19:48 -0300

    Test names.

--- test/test_args.py
+++ test/test_args.py
@@ -9,10 +9,10 @@ class TestArgs(unittest.TestCase):
         with self.assertRaises(SystemExit):
             parse_args([])
 
-    def test_parse_doesnt_fail_with_text_short(self):
+    def test_parse_text_short(self):
         args = parse_args(["-t", "some_text"])
         self.assertEquals("some_text", args.text)
 
-    def test_parse_doesnt_fail_with_text_long(self):
+    def test_parse_text_long(self):
         args = parse_args(["--text", "some_text"])
         self.assertEquals("some_text", args.text)
