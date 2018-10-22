5005df2c94b9440da38ceb7cb36b316f008bb82f
Federico Barrios <fbarrios@fi.uba.ar>
2018-10-22 19:27:49 -0300

    Summary modes and missing text tests.

--- test/test_args.py
+++ test/test_args.py
@@ -1,18 +1,38 @@
 import unittest
 
-from summa.textrank import parse_args
+from summa.textrank import parse_args, SENTENCE, WORD
 
 
 class TestArgs(unittest.TestCase):
 
-    def test_parse_fails_with_no_text(self):
+    def test_parse_fails_with_no_text_option(self):
         with self.assertRaises(SystemExit):
             parse_args([])
 
+    def test_parse_fails_with_no_text_short(self):
+        with self.assertRaises(SystemExit):
+            parse_args(["-t"])
+
+    def test_parse_fails_with_no_text_long(self):
+        with self.assertRaises(SystemExit):
+            parse_args(["--text"])
+
     def test_parse_text_short(self):
         args = parse_args(["-t", "some_text"])
-        self.assertEquals("some_text", args.text)
+        self.assertEqual("some_text", args.text)
 
     def test_parse_text_long(self):
         args = parse_args(["--text", "some_text"])
-        self.assertEquals("some_text", args.text)
+        self.assertEqual("some_text", args.text)
+
+    def test_summary_mode_is_default(self):
+        args = parse_args(["-t", "some_text"])
+        self.assertEqual(SENTENCE, args.summary)
+
+    def test_summary_mode_can_be_overriden(self):
+        args = parse_args(["-t", "some_text", "-s", str(SENTENCE)])
+        self.assertEqual(SENTENCE, args.summary)
+
+    def test_keyword_mode(self):
+        args = parse_args(["-t", "some_text", "-s", str(WORD)])
+        self.assertEqual(WORD, args.summary)
