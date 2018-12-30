521d964e980d997b56b6d7f26f8d197bdc43b4ac
CrossNox <ijmermet@gmail.com>
2018-12-30 11:17:29 -0300

    Migration to argparse (#42)
    * migration to argparse
    
    * Extract parse_args method for testing.
    
    * Adding description to parser. Text is mandatory.
    
    * Test -t is mandatory.
    
    * Test names.
    
    * Summary modes and missing text tests.
    
    * Ratio argparse:
    
    - Fix error message.
    - Adding test.
    - Extracting default value as constant.
    
    * Adding words test. Fix type.
    
    * Adding tests for additional stopwords.
    
    * Redirect stderr to avoid showing usage messages to the screen.
    
    * Using decorators instead of ugly hack.
    
    * Fix missing additional_stopwords parameter.

--- summa/textrank.py
+++ summa/textrank.py
@@ -1,4 +1,6 @@
-import sys, getopt, os
+import os, sys
+import argparse
+
 
 from .summarizer import summarize
 from .keywords import keywords
@@ -7,86 +9,48 @@ from .keywords import keywords
 SENTENCE = 0
 WORD = 1
 
-
-def exit_with_error(err):
-    print("Error: " + str(err))
-    usage()
-    sys.exit(2)
-
-def get_arguments():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "t:s:r:w:a:h", ["text=", "summary=", "ratio=", "words=", "additional_stopwords=", "help"])
-    except getopt.GetoptError as err:
-        exit_with_error(err)
-
-    path = None
-    summarize_by = SENTENCE
-    ratio = 0.2
-    words = None
-    additional_stopwords = None
-    for o, a in opts:
-        if o in ("-t", "--text"):
-            path = a
-        elif o in ("-h", "--help"):
-            usage()
-            sys.exit()
-        elif o in ("-s", "--summary"):
-            summarize_by = int(a)
-        elif o in ("-w", "--words"):
-            words = int(a)
-        elif o in ("-r", "--ratio"):
-            ratio = float(a)
-        elif o in ("-a", "--additional_stopwords"):
-            additional_stopwords = a
-        else:
-            assert False, "unhandled option"
-
-    if path is None:
-        exit_with_error("-t option is required.")
-
-    return path, summarize_by, ratio, words, additional_stopwords
+DEFAULT_RATIO = 0.2
 
 
-help_text = """Usage: textrank -t FILE
--s UNIT, --summary=UNIT:
-\tType of unit to summarize: sentence (0) or word (1). Default value: 0
-\t0: Sentence. 1: Word
--t FILE, --text=FILE:
-\tPATH to text to summarize
--r RATIO, --ratio=RATIO:
-\tFloat number (0,1] that defines the length of the summary. It's a proportion of the original text. Default value: 0.2.
--w WORDS, --words=WORDS:
-\tNumber to limit the length of the summary. The length option is ignored if the word limit is set.
--a, --additional_stopwords
-\tEither a string of comma separated stopwords or a path to a file which has comma separated stopwords in every line
--h, --help:
-\tprints this help
-"""
-def usage():
-    print(help_text)
-
-
-def textrank(text, summarize_by=SENTENCE, ratio=0.2, words=None, additional_stopwords=None):
+def textrank(text, summarize_by=SENTENCE, ratio=DEFAULT_RATIO, words=None, additional_stopwords=None):
     if summarize_by == SENTENCE:
         return summarize(text, ratio, words, additional_stopwords=additional_stopwords)
     else:
         return keywords(text, ratio, words, additional_stopwords=additional_stopwords)
 
 
-def main():
-    path, summarize_by, ratio, words, additional_stopwords = get_arguments()
+def restricted_float(x):
+    x = float(x)
+    if x < 0.0 or x > 1.0:
+        raise argparse.ArgumentTypeError("{} not in range [0.0, 1.0]".format(x))
+    return x
 
-    with open(path) as file:
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, prog="textrank", description="Extract the most relevant sentences or keywords of a given text using the TextRank algorithm.")
+    parser.add_argument('--text', '-t', type=str, required=True, help="Text to summarize")
+    parser.add_argument('--summary', '-s', type=int, default=0, help="Type of unit to summarize: sentence (0) or word (1)")
+    parser.add_argument('--ratio', '-r', type=restricted_float, default=DEFAULT_RATIO, help="Float number (0,1] that defines the length of the summary. It's a proportion of the original text")
+    parser.add_argument('--words', '-w', type=int, help="Number to limit the length of the summary. The length option is ignored if the word limit is set.")
+    parser.add_argument('--additional_stopwords', '-a', help="Either a string of comma separated stopwords or a path to a file which has comma separated stopwords in every line")
+    return parser.parse_args(args)
+
+
+def main():
+    args = parse_args(sys.argv[1:])
+
+    with open(args.text) as file:
         text = file.read()
 
-    if additional_stopwords:
-        if os.path.exists(additional_stopwords):
-            with open(additional_stopwords) as f:
-                additional_stopwords = { s for l in f for s in l.strip().split(",") }
+    additional_stopwords = None
+    if args.additional_stopwords:
+        if os.path.exists(args.additional_stopwords):
+            with open(args.additional_stopwords) as f:
+                additional_stopwords = {s for l in f for s in l.strip().split(",")}
         else:
-            additional_stopwords = additional_stopwords.split(",")
+            additional_stopwords = args.additional_stopwords.split(",")
 
-    print(textrank(text, summarize_by, ratio, words, additional_stopwords))
+    print(textrank(text, args.summary, args.ratio, args.words, additional_stopwords))
 
 
 if __name__ == "__main__":
---
+++ test/test_args.py
@@ -0,0 +1,71 @@
+import unittest
+from summa.textrank import parse_args, SENTENCE, WORD, DEFAULT_RATIO
+from test.utils import silence_stderr
+
+
+class TestArgs(unittest.TestCase):
+
+    @silence_stderr
+    def test_parse_fails_with_no_text_option(self):
+        with self.assertRaises(SystemExit):
+            parse_args([])
+
+    @silence_stderr
+    def test_parse_fails_with_no_text_short(self):
+        with self.assertRaises(SystemExit):
+            parse_args(["-t"])
+
+    @silence_stderr
+    def test_parse_fails_with_no_text_long(self):
+        with self.assertRaises(SystemExit):
+            parse_args(["--text"])
+
+    def test_parse_text_short(self):
+        args = parse_args(["-t", "some_text"])
+        self.assertEqual("some_text", args.text)
+
+    def test_parse_text_long(self):
+        args = parse_args(["--text", "some_text"])
+        self.assertEqual("some_text", args.text)
+
+    def test_summary_mode_is_default(self):
+        args = parse_args(["-t", "some_text"])
+        self.assertEqual(SENTENCE, args.summary)
+
+    def test_summary_mode_can_be_overriden(self):
+        args = parse_args(["-t", "some_text", "-s", str(SENTENCE)])
+        self.assertEqual(SENTENCE, args.summary)
+
+    def test_keyword_mode(self):
+        args = parse_args(["-t", "some_text", "-s", str(WORD)])
+        self.assertEqual(WORD, args.summary)
+
+    def test_ratio_default(self):
+        args = parse_args(["-t", "some_text"])
+        self.assertEqual(DEFAULT_RATIO, args.ratio)
+
+    @silence_stderr
+    def test_ratio_must_be_positive(self):
+        with self.assertRaises(SystemExit):
+            parse_args(["-t", "some_text", "-r", "-1.0"])
+
+    @silence_stderr
+    def test_ratio_must_be_less_than_1(self):
+        with self.assertRaises(SystemExit):
+            parse_args(["-t", "some_text", "-r", "1.1"])
+
+    def test_words_parameter_short(self):
+        args = parse_args(["-t", "some_text", "-w", "200"])
+        self.assertEqual(200, args.words)
+
+    def test_words_parameter_long(self):
+        args = parse_args(["-t", "some_text", "--words", "200"])
+        self.assertEqual(200, args.words)
+
+    def test_additional_stopwords_short(self):
+        args = parse_args(["-t", "some_text", "-a", "uno dos tres catorce"])
+        self.assertEqual("uno dos tres catorce", args.additional_stopwords)
+
+    def test_additional_stopwords_long(self):
+        args = parse_args(["-t", "some_text", "--additional_stopwords", "uno dos tres catorce"])
+        self.assertEqual("uno dos tres catorce", args.additional_stopwords)
