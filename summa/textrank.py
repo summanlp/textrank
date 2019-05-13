5825116737c11fafc811444a7da268910f5b7fb5
adhamehab <adhaamehab7@gmail.com>
2019-05-13 23:06:53 +0200

    Add new CLI (#67)
    * Add new cli
    
    * Fix bug in logic. Fix PEP 8.

--- summa/textrank.py
+++ summa/textrank.py
@@ -28,18 +28,40 @@ def restricted_float(x):
 
 def parse_args(args):
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, prog="textrank", description="Extract the most relevant sentences or keywords of a given text using the TextRank algorithm.")
-    parser.add_argument('--text', '-t', type=str, required=True, help="Text to summarize")
+    # Old API - left for backward compatibility
+    parser.add_argument('--text', '-t', type=str, help="Text to summarize")
     parser.add_argument('--summary', '-s', type=int, default=0, help="Type of unit to summarize: sentence (0) or word (1)")
     parser.add_argument('--ratio', '-r', type=restricted_float, default=DEFAULT_RATIO, help="Float number (0,1] that defines the length of the summary. It's a proportion of the original text")
     parser.add_argument('--words', '-w', type=int, help="Number to limit the length of the summary. The length option is ignored if the word limit is set.")
     parser.add_argument('--additional_stopwords', '-a', help="Either a string of comma separated stopwords or a path to a file which has comma separated stopwords in every line")
+    # New api
+    parser.add_argument('--summarize', type=str, help="Run textrank to summarize the input text.")
+    parser.add_argument('--keywords', type=str, help="Run textrank to extract keywords from the input text.")
     return parser.parse_args(args)
 
 
 def main():
     args = parse_args(sys.argv[1:])
 
-    with open(args.text) as file:
+    mode = None
+    text = None
+
+    if args.summarize:
+        text = args.summarize
+        mode = SENTENCE
+    elif args.keywords:
+        text = args.keywords
+        mode = WORD
+    elif args.summary:  # Old api
+        text = args.text
+        mode = args.summary
+    else:
+        raise argparse.ArgumentTypeError('Error: --summarize or --keywords is required')
+
+    if text is None:
+        raise argparse.ArgumentTypeError('Error: no text to summarize provided.')
+
+    with open(text) as file:
         text = file.read()
 
     additional_stopwords = None
@@ -50,7 +72,7 @@ def main():
         else:
             additional_stopwords = args.additional_stopwords.split(",")
 
-    print(textrank(text, args.summary, args.ratio, args.words, additional_stopwords))
+    print(textrank(text, mode, args.ratio, args.words, additional_stopwords))
 
 
 if __name__ == "__main__":
